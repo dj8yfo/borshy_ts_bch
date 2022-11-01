7baefed3aceca3c527ca0a4f0a9d4376f87a7ce8
dj8yf0μl <sffhox@gmail.com>
2022-11-01 17:21:59 +0200

    move common parts to common Enum class

--- src/client/empty_enum.ts
+++ src/client/empty_enum.ts
@@ -1,18 +1,13 @@
-export class GameCell {
-	enum: string | undefined;
-	gameCellEmpty: GameCellEmpty | undefined;
-	gameCellTic: GameCellTic | undefined;
-	gameCellTac: GameCellTac | undefined;
+import Enum from './enum';
+
+export class GameCell extends Enum {
+    gameCellEmpty: GameCellEmpty | undefined;
+    gameCellTic: GameCellTic | undefined;
+    gameCellTac: GameCellTac | undefined;
 
 
     constructor(properties: any) {
-        if (Object.keys(properties).length !== 1) {
-            throw new Error('Enum can only take single value');
-        }
-        Object.keys(properties).map((key) => {
-            this[key as keyof typeof this] = properties[key];
-            this.enum = key;
-        });
+	super(properties);
     }
 };
 
@@ -27,35 +22,35 @@ export class GameCellTac {
 
 
 export const GAME_CELL_SCHEMA = new Map<any, any>([
-	[
-		GameCell,
-		{
-			kind: 'enum', field: 'enum', values: [
-				['gameCellEmpty', GameCellEmpty],
-				['gameCellTic', GameCellTic],
-				['gameCellTac', GameCellTac],
-			],
-		},
-	],
-	[
-		GameCellEmpty,
-		{
-			kind: 'struct', fields: [
-			],
-		},
-	],
-	[
-		GameCellTic,
-		{
-			kind: 'struct', fields: [
-			],
-		},
-	],
-	[
-		GameCellTac,
-		{
-			kind: 'struct', fields: [
-			],
-		},
-	]
+    [
+	GameCell,
+	{
+	    kind: 'enum', field: 'enum', values: [
+		['gameCellEmpty', GameCellEmpty],
+		['gameCellTic', GameCellTic],
+		['gameCellTac', GameCellTac],
+	    ],
+	},
+    ],
+    [
+	GameCellEmpty,
+	{
+	    kind: 'struct', fields: [
+	    ],
+	},
+    ],
+    [
+	GameCellTic,
+	{
+	    kind: 'struct', fields: [
+	    ],
+	},
+    ],
+    [
+	GameCellTac,
+	{
+	    kind: 'struct', fields: [
+	    ],
+	},
+    ]
 ]);
